4b3deadf211d46ad005aa4151392e2bf3a06bb42
padilla <padilla@uji.es>
2024-12-25 20:58:13 +0100

    Add examples for Vertex AI endpoint and transport configs
    Introduce `OtherConfigurationEndpoint` (example13) to demonstrate setting a custom API endpoint, and `OtherConfigurationTransport` (example12) to explore transport configurations. Corresponding Bazel build files were also added for both examples.

---
+++ java/example/vertexai/example12/src/main/java/org/iatevale/example/vertextai/example12/OtherConfigurationTransport.java
@@ -0,0 +1,29 @@
+package org.iatevale.example.vertextai.example12;
+
+import com.google.cloud.vertexai.VertexAI;
+import com.google.cloud.vertexai.generativeai.GenerativeModel;
+//import com.google.cloud.vertexai.generativeai.Transport;
+import com.google.cloud.vertexai.api.GenerateContentResponse;
+import java.io.IOException;
+
+public class OtherConfigurationTransport {
+
+    private static final String PROJECT_ID = "<your project id>";
+    private static final String LOCATION = "<location>";
+
+    public static void main(String[] args) throws IOException {
+        // No compila.
+        // No encuentra com.google.cloud.vertexai.generativeai.Transport
+//        try (VertexAI vertexAi = new VertexAI.Builder()
+//                .setProjectId(PROJECT_ID)
+//                .setLocation(LOCATION)
+//                .setTransport(Transport.REST);) {
+//
+//            GenerativeModel model = new GenerativeModel("gemini-pro", vertexAi);
+//
+//            GenerateContentResponse response = model.generateContent("How are you?");
+//            // Do something with the response
+//        }
+    }
+
+}
---
+++ java/example/vertexai/example13/src/main/java/org/iatevale/example/vertextai/example13/OtherConfigurationEndpoint.java
@@ -0,0 +1,30 @@
+package org.iatevale.example.vertextai.example13;
+
+import com.google.cloud.vertexai.VertexAI;
+import com.google.cloud.vertexai.generativeai.GenerativeModel;
+import com.google.cloud.vertexai.api.GenerateContentResponse;
+import java.io.IOException;
+
+public class OtherConfigurationEndpoint {
+
+    private static final String PROJECT_ID = "<your project id>";
+    private static final String LOCATION = "<location>";
+
+    private static final String END_POINT = "<new_endpoint>";
+
+    public static void main(String[] args) throws IOException {
+        try (VertexAI vertexAi = new VertexAI.Builder()
+                .setProjectId(PROJECT_ID)
+                .setLocation(LOCATION)
+                .setApiEndpoint(END_POINT).build()) {
+
+            GenerativeModel model = new GenerativeModel("gemini-pro", vertexAi);
+
+            GenerateContentResponse response = model.generateContent("How are you?");
+            // Do something with the response
+
+        }
+
+    }
+
+}
