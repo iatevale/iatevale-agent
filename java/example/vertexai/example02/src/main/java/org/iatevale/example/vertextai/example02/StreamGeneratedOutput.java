4b19d2424d4a6bd86b9679ea5719112a39a88b67
padilla <padilla@uji.es>
2024-12-26 14:56:36 +0100

    Refactor VertexAI initialization in StreamGeneratedOutput.
    Replaced hardcoded project ID and location with a utility-based builder for initializing VertexAI. This improves code maintainability and reusability by centralizing configuration logic.

--- java/example/vertexai/example02/src/main/java/org/iatevale/example/vertextai/example02/StreamGeneratedOutput.java
+++ java/example/vertexai/example02/src/main/java/org/iatevale/example/vertextai/example02/StreamGeneratedOutput.java
@@ -4,6 +4,7 @@ import com.google.cloud.vertexai.VertexAI;
 import com.google.cloud.vertexai.api.GenerateContentResponse;
 import com.google.cloud.vertexai.generativeai.GenerativeModel;
 import com.google.cloud.vertexai.generativeai.ResponseStream;
+import org.iatevale.example.vertextai.common.VertextaiUtil;
 import org.iatevale.util.conversion.ProtoUtils;
 
 import java.io.IOException;
@@ -18,12 +19,9 @@ import java.io.IOException;
 // }
 public class StreamGeneratedOutput {
 
-    private static final String PROJECT_ID = "<your project id>";
-    private static final String LOCATION = "<location>";
-
     public static void main(String[] args) throws IOException {
 
-        try (VertexAI vertexAi = new VertexAI(PROJECT_ID, LOCATION)) {
+        try (VertexAI vertexAi = VertextaiUtil.vertexBuilder().build()) {
 
             final GenerativeModel model = new GenerativeModel("gemini-pro", vertexAi);
 
