76330a651470603d02b9f6dbae3fcd467261b7bd
padilla <padilla@uji.es>
2024-12-27 12:11:36 +0100

    Refactor system instruction setup and update dependencies
    Replace hardcoded project and location initialization with a utility-based VertexAI builder. Refactor system instruction content for clarity and utilize a response handler for output. Update build dependencies to replace platform util with common VertexAI utilities.

--- java/example/vertexai/example08/src/main/java/org/iatevale/example/vertextai/example08/SystemInstruction.java
+++ java/example/vertexai/example08/src/main/java/org/iatevale/example/vertextai/example08/SystemInstruction.java
@@ -1,32 +1,36 @@
 package org.iatevale.example.vertextai.example08;
 
 import com.google.cloud.vertexai.VertexAI;
+import com.google.cloud.vertexai.api.Content;
 import com.google.cloud.vertexai.api.GenerateContentResponse;
 import com.google.cloud.vertexai.generativeai.ContentMaker;
 import com.google.cloud.vertexai.generativeai.GenerativeModel;
+import com.google.cloud.vertexai.generativeai.ResponseHandler;
+import org.iatevale.example.vertextai.common.VertextaiUtil;
 
 import java.io.IOException;
 
 public class SystemInstruction {
 
-    private static final String PROJECT_ID = "<your project id>";
-    private static final String LOCATION = "<location>";
+    static final private String MODEL_NAME = "gemini-pro";
 
     public static void main(String[] args) throws IOException {
-        try (VertexAI vertexAi = new VertexAI(PROJECT_ID, LOCATION);) {
 
-            GenerativeModel model =
-                    new GenerativeModel.Builder()
-                            .setModelName("gemino-pro")
+        try (VertexAI vertexAi = VertextaiUtil.vertexBuilder().build()) {
+
+            final Content systemInstuction = ContentMaker.fromString(
+                    "You're a helpful assistant that starts all its answers with: \"COOL\""
+            );
+
+            final GenerativeModel model = new GenerativeModel.Builder()
+                            .setModelName(MODEL_NAME)
                             .setVertexAi(vertexAi)
-                            .setSystemInstruction(
-                                    ContentMaker.fromString(
-                                            "You're a helpful assistant that starts all its answers with: \"COOL\"")
-                            )
+                            .setSystemInstruction(systemInstuction)
                             .build();
 
-            GenerateContentResponse response = model.generateContent("How are you?");
-            // Do something with the response
+            final GenerateContentResponse response = model.generateContent("How are you?");
+            System.out.println(ResponseHandler.getText(response));
+
         }
     }
 
