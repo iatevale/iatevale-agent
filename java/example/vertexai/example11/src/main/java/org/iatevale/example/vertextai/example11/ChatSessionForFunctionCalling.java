7f731057843cd7b88f88998ea44fde35acf8b613
padilla <padilla@uji.es>
2024-12-27 12:47:44 +0100

    Remove examples 12 and 13 and update example 11.
    Deleted outdated example12 and example13 code and dependencies, including their BUILD files. Enhanced example11 by introducing utility usage, streamlining code, and improving the prompt and function call logic. Updated the Bazel dependencies for example11 to reflect the changes.

--- java/example/vertexai/example11/src/main/java/org/iatevale/example/vertextai/example11/ChatSessionForFunctionCalling.java
+++ java/example/vertexai/example11/src/main/java/org/iatevale/example/vertextai/example11/ChatSessionForFunctionCalling.java
@@ -5,6 +5,7 @@ import com.google.cloud.vertexai.api.Content;
 import com.google.cloud.vertexai.api.GenerateContentResponse;
 import com.google.cloud.vertexai.api.Tool;
 import com.google.cloud.vertexai.generativeai.*;
+import org.iatevale.example.vertextai.common.VertextaiUtil;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -12,102 +13,69 @@ import java.util.Collections;
 
 public class ChatSessionForFunctionCalling {
 
-    private static final String PROJECT_ID = "<your project>";
-    private static final String LOCATION = "<location>";
     private static final String MODEL_NAME = "gemini-pro";
-    private static final String TEXT = "What's the weather in Vancouver?";
+
+    private static final String JSON_STRING_FUNCTION = """
+              {
+                "name": "getCurrentWeather",
+                "description": "Get the current weather in a given location",
+                "parameters": {
+                  "type": "OBJECT",
+                  "properties": {
+                    "location": {
+                      "type": "STRING",
+                      "description": "location"
+                    }
+                  }
+                }
+              }
+            """;
+
+    private static final String PROMPT = "What's the weather in Vancouver?";
 
     public static void main(String[] args) throws IOException {
-        try (VertexAI vertexAi = new VertexAI(PROJECT_ID, LOCATION); ) {
+
+        try (VertexAI vertexAi = VertextaiUtil.vertexBuilder().build()) {
+
             // Declare a function to be used in a request.
             // We construct a jsonString that corresponds to the following function
             // declaration.
-            // {
-            //   "name": "getCurrentWeather",
-            //   "description": "Get the current weather in a given location",
-            //   "parameters": {
-            //     "type": "OBJECT",
-            //     "properties": {
-            //       "location": {
-            //         "type": "STRING",
-            //         "description": "location"
-            //       }
-            //     }
-            //   }
-            // }
-            // With JDK 15 and above, you can do
-            //
-            // String jsonString = """
-            //   {
-            //     "name": "getCurrentWeather",
-            //     "description": "Get the current weather in a given location",
-            //     "parameters": {
-            //       "type": "OBJECT",
-            //       "properties": {
-            //         "location": {
-            //           "type": "STRING",
-            //           "description": "location"
-            //         }
-            //       }
-            //     }
-            //   }
-            //  """
-            String jsonString =
-                    "{\n"
-                            + " \"name\": \"getCurrentWeather\",\n"
-                            + " \"description\": \"Get the current weather in a given location\",\n"
-                            + " \"parameters\": {\n"
-                            + "   \"type\": \"OBJECT\", \n"
-                            + "   \"properties\": {\n"
-                            + "     \"location\": {\n"
-                            + "       \"type\": \"STRING\",\n"
-                            + "       \"description\": \"location\"\n"
-                            + "     }\n"
-                            + "   }\n"
-                            + " }\n"
-                            + "}";
-            Tool tool =
-                    Tool.newBuilder()
-                            .addFunctionDeclarations(
-                                    FunctionDeclarationMaker.fromJsonString(jsonString)
-                            )
-                            .build();
+            final Tool tool = Tool.newBuilder()
+                .addFunctionDeclarations(FunctionDeclarationMaker.fromJsonString(JSON_STRING_FUNCTION))
+                .build();
 
-            // Start a chat session from a model, with the use of the declared
-            // function.
-            GenerativeModel model =
-                    new GenerativeModel.Builder()
-                            .setModelName(MODEL_NAME)
-                            .setVertexAi(vertexAi)
-                            .setTools(Arrays.asList(tool))
-                            .build();
-            ChatSession chat = model.startChat();
+            // Se cre el modelo con la funcion configurada
+            final GenerativeModel model = new GenerativeModel.Builder()
+                .setModelName(MODEL_NAME)
+                .setVertexAi(vertexAi)
+                .setTools(Arrays.asList(tool))
+                .build();
 
-            System.out.println(String.format("Ask the question: %s", TEXT));
-            GenerateContentResponse response = chat.sendMessage(TEXT);
+            // Se crea el chat
+            final ChatSession chat = model.startChat();
 
-            // The model will most likely return a function call to the declared
-            // function `getCurrentWeather` with "Vancouver" as the value for the
-            // argument `location`.
-            System.out.println("\nPrint response: ");
+            // Peticion al modelo a traves del chat
+            System.out.println(String.format("Ask the question: %s", PROMPT));
+            GenerateContentResponse response = chat.sendMessage(PROMPT);
+            System.out.println("Respuesta del modelo: ");
+            System.out.println("===========================");
             System.out.println(ResponseHandler.getContent(response));
             System.out.println("\n");
 
-            // Provide an answer to the model so that it knows what the result of a
-            // "function call" is.
-            Content content =
-                    ContentMaker.fromMultiModalData(
-                            PartMaker.fromFunctionResponse(
-                                    "getCurrentWeather", Collections.singletonMap("currentWeather", "snowing")));
-            System.out.println("Provide the function response: ");
+            // Se le proporciona una respuesta al modelo para que este conozca el resultado de la "function call"
+            final Content content = ContentMaker.fromMultiModalData(
+                PartMaker.fromFunctionResponse("getCurrentWeather", Collections.singletonMap("currentWeather", "snowing"))
+            );
+            System.out.println("Le proporcionamos la respuesta: ");
             System.out.println(content);
             System.out.println("\n");
             response = chat.sendMessage(content);
 
             // See what the model replies now
-            System.out.println("\nPrint response: ");
+            System.out.println("\nDe nuevo respuesta del modelo:: ");
             System.out.println(ResponseHandler.getText(response));
             System.out.println("\n");
+            
         }
     }
 
