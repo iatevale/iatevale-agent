f6110b69908b6bb1a023fb890448ca2a6278a295
padilla <padilla@uji.es>
2024-12-27 12:21:39 +0100

    Refactor VertexAI model setup and update dependencies.
    Replaced hardcoded configuration with a utility-based builder for `VertexAI`. Updated model initialization to use the fluent API, improving safety configuration and content generation. Adjusted dependencies in BUILD.bazel for better modularity.

--- java/example/vertexai/example09/src/main/java/org/iatevale/example/vertextai/example09/RequestLevelConfigurations.java
+++ java/example/vertexai/example09/src/main/java/org/iatevale/example/vertextai/example09/RequestLevelConfigurations.java
@@ -1,35 +1,44 @@
 package org.iatevale.example.vertextai.example09;
 
 import com.google.cloud.vertexai.VertexAI;
+import com.google.cloud.vertexai.api.GenerateContentResponse;
 import com.google.cloud.vertexai.api.HarmCategory;
 import com.google.cloud.vertexai.api.SafetySetting;
 import com.google.cloud.vertexai.api.SafetySetting.HarmBlockThreshold;
+import com.google.cloud.vertexai.generativeai.GenerativeModel;
+import com.google.cloud.vertexai.generativeai.ResponseHandler;
+import org.iatevale.example.vertextai.common.VertextaiUtil;
 
 import java.io.IOException;
+import java.util.List;
 
+// Construye la instancia de GenerativeModel mediante un builder el cual
+// permite proporcioarle configuracion de seguridad.
+// En este caso ademas utilizo el interface fluido para generar
+// en una sola sentencia la respuesta.
 public class RequestLevelConfigurations {
 
-    private static final String PROJECT_ID = "<PROJECT_ID>";
-    private static final String LOCATION = "<LOCATION>";
+    static final private String MODEL_NAME = "gemini-pro";
 
     public static void main(String[] args) throws IOException {
-        try (VertexAI vertexAi = new VertexAI(PROJECT_ID, LOCATION); ) {
+
+        try (VertexAI vertexAi = VertextaiUtil.vertexBuilder().build()) {
+
             // Build a SafetySetting instance.
-            SafetySetting safetySetting =
-                    SafetySetting.newBuilder()
-                            .setCategory(HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT)
-                            .setThreshold(HarmBlockThreshold.BLOCK_LOW_AND_ABOVE)
-                            .build();
+            final SafetySetting safetySetting = SafetySetting.newBuilder()
+                .setCategory(HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT)
+                .setThreshold(HarmBlockThreshold.BLOCK_LOW_AND_ABOVE)
+                .build();
 
+            final GenerateContentResponse response = new GenerativeModel.Builder()
+                    .setVertexAi(vertexAi)
+                    .setModelName(MODEL_NAME)
+                    .setSafetySettings(List.of(safetySetting))
+                    .build()
+                    .generateContent("How are you?");
 
-            // Generate the response with the fluent API `withSafetySetting`.
-            // TODO: Las tres siguientes lineas no compilan por que el ejemplo
-            // delrepositorio esta mal -> https://github.com/googleapis/google-cloud-java/tree/main/java-vertexai#add-dependency
-//            GenerateContentResponse response = model
-//                            .withSafetySetting(Arrays.asList(SafetySetting))
-//                            .generateContent("Please explain LLM?");
+            System.out.println(ResponseHandler.getText(response));
 
-            // Do something with the response.
         }
     }
 
