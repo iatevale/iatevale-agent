d93e10e01969404700cd377593450b3a66f47bbd
padilla <padilla@uji.es>
2024-12-27 12:52:23 +0100

    Remove deployment-related Bazel and shell scripts.
    The deployment process was consolidated and simplified by removing redundant Bazel build configurations and shell scripts. Necessary deployment logic for "iatevaleagent" and "datastore-example" has been updated or relocated, streamlining the process and reducing duplication.

--- deploy/iatevaleagent/deploy.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-bazel run //deploy:datastore_example_gcloud_push
-gcloud run deploy datastore-example-service \
-    --image=gcr.io/<TU_PROYECTO_ID>/datastore-example-image \
-    --platform=managed \
-    --allow-unauthenticated
