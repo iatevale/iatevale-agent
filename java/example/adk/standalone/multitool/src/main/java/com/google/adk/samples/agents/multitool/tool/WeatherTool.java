ad76b3e7a2b05330b5dfcbb96b346f0b999e57ef
padilla <padilla@speedycontrol.com>
2025-06-02 13:44:16 +0200

    Refactor `WeatherTool` to extend `AbstractToolBuilder`, streamline initialization with `instantiate` method, modularize weather retrieval logic, and update dependencies in `BUILD.bazel` for improved maintainability.

--- java/example/adk/standalone/multitool/src/main/java/com/google/adk/samples/agents/multitool/tool/WeatherTool.java
+++ java/example/adk/standalone/multitool/src/main/java/com/google/adk/samples/agents/multitool/tool/WeatherTool.java
@@ -1,25 +1,41 @@
 package com.google.adk.samples.agents.multitool.tool;
 
 import com.google.adk.tools.Annotations;
+import com.google.adk.tools.BaseTool;
+import com.google.adk.tools.FunctionTool;
+import org.iatevale.adk.common.tool.AbstractToolBuilder;
 
 import java.util.Map;
 
-public class WeatherTool {
+public class WeatherTool extends AbstractToolBuilder {
 
-    public static Map<String, String> getWeather(
-            @Annotations.Schema(description = "The name of the city for which to retrieve the weather report")
-            String city) {
-        if (city.equalsIgnoreCase("new york")) {
-            return Map.of(
-                    "status",
-                    "success",
-                    "report",
-                    "The weather in New York is sunny with a temperature of 25 degrees Celsius (77 degrees Fahrenheit).");
+    static public WeatherTool instantiate() {
+        FunctionTool functionTool = FunctionTool.create(WeatherToolImpl.class, "getWeather");
+        return new WeatherTool(functionTool);
+    }
 
-        } else {
-            return Map.of(
-                    "status", "error", "report", "Weather information for " + city + " is not available.");
+    static private class WeatherToolImpl {
+
+        public static Map<String, String> getWeather(
+                @Annotations.Schema(description = "The name of the city for which to retrieve the weather report")
+                String city) {
+            if (city.equalsIgnoreCase("new york")) {
+                return Map.of(
+                        "status",
+                        "success",
+                        "report",
+                        "The weather in New York is sunny with a temperature of 25 degrees Celsius (77 degrees Fahrenheit).");
+
+            } else {
+                return Map.of(
+                        "status", "error", "report", "Weather information for " + city + " is not available.");
+            }
         }
+
+    }
+
+    public WeatherTool(BaseTool tool) {
+        super(tool);
     }
 
 }
