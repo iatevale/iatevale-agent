276a66b69ae84750a5fd97120c45dbe95526ae3b
padilla <padilla@uji.es>
2025-06-03 20:12:26 +0200

    Add `main` method to `TelegrambotsExample` with basic output
    Introduced a `main` method in `TelegrambotsExample` as an entry point displaying a simple "Hello Telegrambots!" message.

--- java/example/telegrambots/src/main/java/org/iatevale/example/telegrambots/TelegrambotsExample.java
+++ java/example/telegrambots/src/main/java/org/iatevale/example/telegrambots/TelegrambotsExample.java
@@ -1,4 +1,9 @@
 package org.iatevale.example.telegrambots;
 
 public class TelegrambotsExample {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Telegrambots!");
+    }
+
 }
