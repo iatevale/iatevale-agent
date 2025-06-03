92bfe4d1f768b56591f6474fa7c32a0e307802cc
padilla <padilla@uji.es>
2025-06-03 20:15:41 +0200

    Add `BotImpl` class and update Bazel dependencies for Telegram Bots integration
    Introduced a new `BotImpl` class implementing `LongPollingSingleThreadUpdateConsumer` with basic method stubs. Updated `BUILD.bazel` to include `telegrambots-meta` dependency.

---
+++ java/example/telegrambots/src/main/java/org/iatevale/example/telegrambots/impl/BotImpl.java
@@ -0,0 +1,20 @@
+package org.iatevale.example.telegrambots.impl;
+
+import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import java.util.List;
+
+public class BotImpl implements LongPollingSingleThreadUpdateConsumer {
+
+    @Override
+    public void consume(List<Update> updates) {
+        LongPollingSingleThreadUpdateConsumer.super.consume(updates);
+    }
+
+    @Override
+    public void consume(Update update) {
+
+    }
+
+}
