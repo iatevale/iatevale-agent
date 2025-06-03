c7d62acc3e701787bbeeb9eae9874f6954ebe329
padilla <padilla@uji.es>
2025-06-03 20:19:50 +0200

    Enhance `TelegrambotsExample` with bot initialization and message handling
    Replaced placeholder `main` method in `TelegrambotsExample` with a working implementation that starts `MyAmazingBot` and registers it with `TelegramBotsLongPollingApplication`. Added `MyAmazingBot` class to handle updates and send messages using `OkHttpTelegramClient`.

--- java/example/telegrambots/src/main/java/org/iatevale/example/telegrambots/TelegrambotsExample.java
+++ java/example/telegrambots/src/main/java/org/iatevale/example/telegrambots/TelegrambotsExample.java
@@ -1,9 +1,22 @@
 package org.iatevale.example.telegrambots;
 
+import org.iatevale.example.telegrambots.impl.MyAmazingBot;
+import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
+
 public class TelegrambotsExample {
 
     public static void main(String[] args) {
-        System.out.println("Hello Telegrambots!");
-    }
 
+        String botToken = "12345:YOUR_TOKEN";
+        // Using try-with-resources to allow autoclose to run upon finishing
+        try (TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication()) {
+            botsApplication.registerBot(botToken, new MyAmazingBot(botToken));
+            System.out.println("MyAmazingBot successfully started!");
+            // Ensure this prcess wait forever
+            Thread.currentThread().join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 }
